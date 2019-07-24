8bbfc3b1da065d6f02c5b8b0ace04d8ef73fba4d
nvdxavier <nvdxavier@gmail.com>
2019-07-25 01:10:53 +0200

    test

--- src/Form/PlaylistType.php
+++ src/Form/PlaylistType.php
@@ -13,8 +13,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
-//use Symfony\Component\Form\Extension\Core\Type\CollectionType;
-
 class PlaylistType extends AbstractType
 {
     /**
